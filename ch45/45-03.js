1f827091be6fc221fc561bf6db8dfad54af01eea
kshelp <kshelp@naver.com>
2022-08-21 22:47:08 +0900

    .

---
+++ ch45/45-03.js
@@ -0,0 +1,24 @@
+// XMLHttpRequest is a built-in object in web browsers.
+// If you really want to use XHR in Node.js then, install xhrs.
+// c:\>npm install xhr2
+const XMLHttpRequest = require('xhr2');
+
+// GET 요청을 위한 비동기 함수
+const get = url => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+  
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        // ① 서버의 응답을 반환한다.
+        return JSON.parse(xhr.response);
+      }
+      console.error(`${xhr.status} ${xhr.statusText}`);
+    };
+  };
+  
+  // ② id가 1인 post를 취득
+  const response = get('https://jsonplaceholder.typicode.com/posts/1');
+  console.log(response); // undefined
+  
