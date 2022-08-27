2e830d4ad34c16531e6602ca54473b8edfc966ea
kshelp <kshelp@naver.com>
2022-08-27 23:13:17 +0900

    .

---
+++ ch06/06-10.js
@@ -0,0 +1,19 @@
+var template = '<ul>\n\t<li><a href="#">Home</a></li>\n</ul>';
+
+console.log(template);
+/*
+<ul>
+  <li><a href="#">Home</a></li>
+</ul>
+*/
+
+var template = `<ul>
+  <li><a href="#">Home</a></li>
+</ul>`;
+
+console.log(template);
+/*
+<ul>
+  <li><a href="#">Home</a></li>
+</ul>
+*/
---
+++ ch06/06-12.js
@@ -0,0 +1,15 @@
+var first = 'Ung-mo';
+var last = 'Lee';
+
+// ES5: 문자열 연결
+console.log('My name is ' + first + ' ' + last + '.'); // My name is Ung-mo Lee.
+
+var first = 'Ung-mo';
+var last = 'Lee';
+
+// ES6: 표현식 삽입
+console.log(`My name is ${first} ${last}.`); // My name is Ung-mo Lee.
+
+console.log(`1 + 2 = ${1 + 2}`); // 1 + 2 = 3
+
+console.log('1 + 2 = ${1 + 2}'); // 1 + 2 = ${1 + 2}
